32ddae6c3d26ed08480dafbe1f4c035e3289b142
Mahmoud Al Ali <119513808+mahmoud98alali@users.noreply.github.com>
2023-01-21 16:36:31 +0400

    category

--- lib/presentation/screens/store_by_category_screen.dart
+++ lib/presentation/screens/store_by_category_screen.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ghaf_application/app/preferences/shared_pref_controller.dart';
-import 'package:ghaf_application/domain/model/product2.dart';
 import 'package:ghaf_application/presentation/resources/routes_manager.dart';
 import 'package:ghaf_application/providers/category_provider.dart';
 import 'package:ghaf_application/providers/product_provider.dart';
 import 'package:provider/provider.dart';
-
+import 'package:ghaf_application/domain/model/product2.dart';
 import '../resources/assets_manager.dart';
 import '../resources/color_manager.dart';
 import '../resources/font_manager.dart';
@@ -49,11 +48,20 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
 
   @override
   void initState() {
+
     Provider.of<CategoryProvider>(context, listen: false)
         .getStoreForCategory(widget.productInf['id'])
         .then((value) => isLoading = false);
 
-
+    // Provider.of<CategoryProvider>(context, listen: false)
+    //     .getStoreForCategory(widget.productInf['id'])
+    //     .then((value) => Provider.of<ProductProvider>(context, listen: false)
+    //         .getDurationGoogleMap(
+    //             LatOne: 24.600661,
+    //             LonOne: 54.935448,
+    //             LatTow: 24.400661,
+    //             LonTow: 54.635448))
+    //     .then((value) => isLoading = false);
 
     // ############################################
     //get all information from latitude and longitude
@@ -61,7 +69,6 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
     // GetAddressFromLatLong(LatLng(myLocationLat,myLocationLong));
     // ##############################################
     // ##############################################
-
     super.initState();
   }
 
@@ -72,15 +79,15 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
           LatOne: 24.600661,
           LonOne: 54.935448,
           LatTow:
-              double.parse(product2[i].branch!['branchAddress']['altitude']) ??
-                  24.400661,
+          double.parse(product2[i].branch!['branchAddress']['altitude']) ??
+              24.400661,
           LonTow:
-              double.parse(product2[i].branch!['branchAddress']['longitude']) ??
-                  54.635448).then((value) => list.add({
+          double.parse(product2[i].branch!['branchAddress']['longitude']) ??
+              54.635448).then((value) => list.add({
         'distance' : Provider.of<ProductProvider>(context,listen: false).distance,
-         'duration': Provider.of<ProductProvider>(context,listen: false).duration
+        'duration': Provider.of<ProductProvider>(context,listen: false).duration
       })).then((value) => null);
-       ;
+      ;
     }
 
     for (int i = 0; i< list.length; i++ ) {
@@ -98,17 +105,7 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
         _Duration = list[i]['duration'];
       }
 
-    }
-
-    // _listDuration = list;
-    // _listDuration.sort((a, b) {
-    //   a['distance'].
-    // },);
-    print('====================================list of duration');
-    print(_Duration);
-
-  }
-
+    }}
   @override
   void didChangeDependencies() {
     // duration = Provider
@@ -130,6 +127,7 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
     var storeName = Provider.of<CategoryProvider>(context).store;
     var storeid = Provider.of<CategoryProvider>(context).storeId;
 
+    var duration = Provider.of<ProductProvider>(context).duration;
 
     print('=======================================duration');
     print(duration);
@@ -146,6 +144,7 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
             height: MediaQuery.of(context).size.height * 1,
             child: Column(
               children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -172,7 +171,9 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
                   ],
                 ),
                 isLoading
-                    ? Center(
+                    ? Padding(
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height / 2),
                         child: Container(
                           width: 20.h,
                           height: 20.h,
@@ -182,7 +183,9 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
                         ),
                       )
                     : store.length == 0
-                        ? Center(
+                        ? Padding(
+                            padding: EdgeInsets.only(
+                                top: MediaQuery.of(context).size.height / 2),
                             child: Text('No Product Found'),
                           )
                         : ListView.builder(
@@ -198,111 +201,138 @@ class _StoreByCategryScreenState extends State<StoreByCategryScreen> {
                                       });
                                 },
                                 child: Card(
-                                  elevation: 3,
                                   child: Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: ListTile(
-                                      leading: Image.asset(
-                                          'assets/images/logo1.png'),
-                                      title: Text(
-                                          store[index1].branch?['storeName'] ??
-                                              'sasa'),
-                                      subtitle: Column(
-                                        children: [
-                                          Row(
-                                            children: [
-                                              Icon(
-                                                Icons.star,
-                                                color: Colors.yellow,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              store[0].storeStars == null
-                                                  ? Text(
-                                                      '0.0',
-                                                      style: getSemiBoldStyle(
-                                                        color: ColorManager
-                                                            .primaryDark,
-                                                        fontSize: FontSize.s18,
-                                                      ),
-                                                    )
-                                                  : Text(
-                                                      '${store[0].storeStars.toString()}.0',
-                                                      style: getSemiBoldStyle(
-                                                        color: ColorManager
-                                                            .primaryDark,
-                                                        fontSize: FontSize.s18,
-                                                      ),
-                                                    ),
-                                              SizedBox(
-                                                width: 30,
-                                              ),
-                                              Text(
-                                                '(${store[0].storeReviewCount.toString()})',
-                                                style: getSemiBoldStyle(
-                                                  color:
-                                                      ColorManager.primaryDark,
-                                                  fontSize: FontSize.s14,
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/logo1.png',
+                                          height: 100,
+                                          width: 100,
+                                        ),
+
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text(
+                                                  store[index1]
+                                                          .branch?['storeName'] ??
+                                                      'sasa',
+                                                  style: TextStyle(fontSize: 15.0),
                                                 ),
-                                              ),
-                                            ],
-                                          ),
-                                          Container(
-                                            // width: 200,
-                                            height: MediaQuery.of(context)
-                                                    .size
-                                                    .height *
-                                                0.1,
-                                            child: ListView.builder(
-                                              scrollDirection: Axis.horizontal,
-                                              shrinkWrap: true,
-                                              itemCount: (store[0].branch?[
-                                                      'storeDeliveryCost'])
-                                                  .length,
-                                              itemBuilder: (context, index) {
-                                                return Row(
+                                                SizedBox(width: 11,),
+                                                Row(
                                                   children: [
-                                                    Image.network(store[0]
-                                                                .branch?[
-                                                            'storeDeliveryCost']
-                                                        [index]['methodImage']),
-                                                    SizedBox(
-                                                      width: 10,
+                                                    Icon(
+                                                      Icons.timer,
+                                                      color: ColorManager.primary,
                                                     ),
-                                                    Text((store[0].branch?[
-                                                                'storeDeliveryCost']
-                                                            [index]['cost'])
-                                                        .toString()),
                                                     SizedBox(
-                                                      width: 10,
+                                                      width: 4,
                                                     ),
+                                                    Text(_Duration ??
+                                                        0.toString()),
                                                   ],
-                                                );
-                                              },
+                                                ),
+                                              ],
                                             ),
-                                          ),
-                                          Row(
-                                            children: [
-                                              Icon(
-                                                Icons.timer,
-                                                color: ColorManager.primary,
+                                            SizedBox(height: 2,),
+                                            Row(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                Icon(
+                                                  Icons.star,
+                                                  color: Colors.yellow,
+                                                ),
+                                                SizedBox(
+                                                  width: 3,
+                                                ),
+                                                store[0].storeStars == null
+                                                    ? Text(
+                                                  '0.0',
+                                                  style: getSemiBoldStyle(
+                                                    color: ColorManager
+                                                        .primaryDark,
+                                                    fontSize: FontSize.s18,
+                                                  ),
+                                                )
+                                                    : Text(
+                                                  '${store[0].storeStars.toString()}.0',
+                                                  style: getSemiBoldStyle(
+                                                    color: ColorManager
+                                                        .primaryDark,
+                                                    fontSize: FontSize.s18,
+                                                  ),
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Text(
+                                                  '(${store[0].storeReviewCount.toString()})',
+                                                  style: getSemiBoldStyle(
+                                                    color:
+                                                    ColorManager.primaryDark,
+                                                    fontSize: FontSize.s14,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                           Column(
+                                                children: [
+                                                  Container(
+                                                    // width: 200,
+                                                    height:
+                                                        MediaQuery.of(context).size.height *
+                                                            0.1,
+                                                    child: ListView.builder(
+                                                      scrollDirection: Axis.horizontal,
+                                                      shrinkWrap: true,
+                                                      itemCount: (store[0]
+                                                              .branch?['storeDeliveryCost'])
+                                                          .length,
+                                                      itemBuilder: (context, index) {
+                                                        return Row(
+                                                          children: [
+                                                            Image.network(store[0].branch?[
+                                                                    'storeDeliveryCost']
+                                                                [index]['methodImage']),
+                                                            SizedBox(
+                                                              width: 4,
+                                                            ),
+                                                            Text((store[0].branch?[
+                                                                        'storeDeliveryCost']
+                                                                    [index]['cost'])
+                                                                .toString()),
+                                                            SizedBox(
+                                                              width: 4,
+                                                            ),
+                                                          ],
+                                                        );
+                                                      },
+                                                    ),
+                                                  ),
+                                                ],
                                               ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text(_Duration ??
-                                                  0.toString())
-                                            ],
-                                          ),
-                                        ],
-                                      ),
+
+                                          ],
+                                        ),
+
+                                      ],
                                     ),
+
                                   ),
                                 ),
                               );
                             },
-                          ),
+                          )
               ],
             ),
           ),
