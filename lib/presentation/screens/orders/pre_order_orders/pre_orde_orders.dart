ce4cfdca067173acec33b0af45d6591b7b96f83e
aliKAdon <alikadonn@gmail.com>
2023-04-10 15:54:00 +0400

    edit 104

--- lib/presentation/screens/orders/pre_order_orders/pre_orde_orders.dart
+++ lib/presentation/screens/orders/pre_order_orders/pre_orde_orders.dart
@@ -40,124 +40,127 @@ class _PreOrderOrdersState extends State<PreOrderOrders> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GetBuilder<CheckOutGetxController>(
-        builder: (controller) =>  controller.preOrders.isEmpty
-            ? Center(
-          child: Text(AppLocalizations.of(context)!.no_product_found,
-              style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: FontSize.s18,
-                  color: ColorManager.primary)),
-        )
-            : Column(
-          children: [
-            SizedBox(
-                height: MediaQuery.of(context).size.height * 0.06),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0, right: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Image.asset(
-                      IconsAssets.arrow,
-                      height: AppSize.s18,
-                      width: AppSize.s10,
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: GetBuilder<CheckOutGetxController>(
+          builder: (controller) =>  controller.preOrders.isEmpty
+              ? Center(
+            child: Text(AppLocalizations.of(context)!.no_product_found,
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: FontSize.s18,
+                    color: ColorManager.primary)),
+          )
+              : Column(
+            children: [
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.06),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0, right: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Image.asset(
+                        IconsAssets.arrow,
+                        height: AppSize.s18,
+                        width: AppSize.s10,
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  Text(
-                    AppLocalizations.of(context)!.pre_order,
-                    style: getSemiBoldStyle(
-                      color: ColorManager.primaryDark,
-                      fontSize: FontSize.s18,
+                    Spacer(),
+                    Text(
+                      AppLocalizations.of(context)!.pre_order,
+                      style: getSemiBoldStyle(
+                        color: ColorManager.primaryDark,
+                        fontSize: FontSize.s18,
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                ],
+                    Spacer(),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01),
-            Divider(
-              color: ColorManager.greyLight,
-              thickness: 1,
-            ),
-            ListView.builder(
-              physics: BouncingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: controller.preOrders.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    // Navigator.of(context).push(MaterialPageRoute(
-                    //   builder: (context) => OrderTrackingScreen(
-                    //       orderId:
-                    //       controller.preOrders[index].id!,
-                    //       source: controller
-                    //           .preOrders[index].deliveryPoint!,
-                    //       destination: controller
-                    //           .preOrders[index]
-                    //           .branch!
-                    //           .branchAddress!),
-                    // ));
-                  },
-                  child: Column(
-                    children: [
-                      MyOrdersWidget(
-                          image: controller.preOrders[index].branch!.branchLogoImage!,
-                          statusName: controller
-                              .preOrders[index].statusName!,
-                          date: controller
-                              .preOrders[index].createDate!,
-                          price: (controller.preOrders[index]
-                              .orderCostForCustomer)
-                              .toString(),
-                          orderSequence: (controller
-                              .preOrders[index]
-                              .sequenceNumber)
-                              .toString(),
-                          branchName: controller
-                              .preOrders[index]
-                              .branch!
-                              .storeName!,
-                          branchAddress: controller
-                              .preOrders[index]
-                              .branch!
-                              .branchAddress ??
-                              null),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            right: 12.0, left: 12.0),
-                        child: Divider(
-                          thickness: 1,
-                          color: ColorManager.greyLight,
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
-            // Container(
-            //   height: MediaQuery.of(context).size.height * 0.08,
-            //   padding: EdgeInsets.all(5),
-            //   child: ElevatedButton(
-            //     onPressed: () {},
-            //     child: Row(
-            //       children: [
-            //         Text('30 AED'),
-            //         Spacer(),
-            //         Icon(Icons.shopping_bag_outlined),
-            //         Text(AppLocalizations.of(context)!.add_to_cart)
-            //       ],
-            //     ),
-            //   ),
-            // )
-          ],
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01),
+              Divider(
+                color: ColorManager.greyLight,
+                thickness: 1,
+              ),
+              ListView.builder(
+                physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: controller.preOrders.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Navigator.of(context).push(MaterialPageRoute(
+                      //   builder: (context) => OrderTrackingScreen(
+                      //       orderId:
+                      //       controller.preOrders[index].id!,
+                      //       source: controller
+                      //           .preOrders[index].deliveryPoint!,
+                      //       destination: controller
+                      //           .preOrders[index]
+                      //           .branch!
+                      //           .branchAddress!),
+                      // ));
+                    },
+                    child: Column(
+                      children: [
+                        MyOrdersWidget(
+                            image: controller.preOrders[index].branch!.branchLogoImage!,
+                            statusName: controller
+                                .preOrders[index].statusName!,
+                            date: controller
+                                .preOrders[index].createDate!,
+                            price: (controller.preOrders[index]
+                                .orderCostForCustomer)
+                                .toString(),
+                            orderSequence: (controller
+                                .preOrders[index]
+                                .sequenceNumber)
+                                .toString(),
+                            branchName: controller
+                                .preOrders[index]
+                                .branch!
+                                .storeName!,
+                            branchAddress: controller
+                                .preOrders[index]
+                                .branch!
+                                .branchAddress ??
+                                null),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 12.0, left: 12.0),
+                          child: Divider(
+                            thickness: 1,
+                            color: ColorManager.greyLight,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              // Container(
+              //   height: MediaQuery.of(context).size.height * 0.08,
+              //   padding: EdgeInsets.all(5),
+              //   child: ElevatedButton(
+              //     onPressed: () {},
+              //     child: Row(
+              //       children: [
+              //         Text('30 AED'),
+              //         Spacer(),
+              //         Icon(Icons.shopping_bag_outlined),
+              //         Text(AppLocalizations.of(context)!.add_to_cart)
+              //       ],
+              //     ),
+              //   ),
+              // )
+            ],
+          ),
         ),
       ),
     );
